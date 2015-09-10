79cd4377cc0ff08dd619cec8b92ec4515195635f
Danil42Russia <dan.dan.d@yandex.ru>
2015-09-10 15:14:45 +0700

    Update Begin_14.pas

--- Begin/Begin_14.pas
+++ Begin/Begin_14.pas
@@ -1,9 +1,12 @@
 program Begin_14;
-var L,R,S,pi:real;
+
+var
+  L, R, S, pi: real;
+
 begin
-pi:=3.14;
-L:=19.23;
-R:=L/2/pi;
-writeln(R);
-writeln(pi*R*R);
+  pi := 3.14;
+  L := 19.23;
+  R := L / 2 / pi;
+  writeln(R);
+  writeln(pi * R * R);
 end.
